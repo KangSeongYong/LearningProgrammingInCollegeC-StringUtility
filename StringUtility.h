2a6ce36fc2052e3f5ed2a57f14eb9b7c03565546
KangSeongYong <97003669+KangSeongYong@users.noreply.github.com>
2022-09-19 18:28:40 +0900

    Update StringUtility.h
    ㅎ

--- StringUtility.h
+++ StringUtility.h
@@ -2,4 +2,5 @@
 #define STRINGUTILITY_H
 const int is_empty_or_null(const char* string);
 const int is_whitespace_or_null(const char* string);
+const char* index_of_or_null(const char* string, const char ch);
 #endif
